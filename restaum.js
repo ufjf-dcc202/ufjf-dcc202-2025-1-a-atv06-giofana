4a46225ef46240ac6698bb1bf53f67074f1a570d
giovana <giovana.maieli@codejr.com.br>
2025-08-18 23:27:46 -0300

    adiciona verificação de jogada válida

--- restaum.js
+++ restaum.js
@@ -29,4 +29,26 @@ export function getSelecionado(){
 export function tentaMover(destL, destC){
     if (selecionado === null) return false;
     const { l, c } = selecionado;
+
+    if (l === destL && Math.abs(c - destC) === 2) {
+        const meio = (c + destC) / 2;
+        if (tabuleiro[l][meio] === 1 && tabuleiro[destL][destC] === 0) {
+            tabuleiro[l][c] = 0;
+            tabuleiro[l][meio] = 0;
+            tabuleiro[destL][destC] = 1;
+            selecionado = null;
+            return true;
+        }
+    }
+    if (c === destC && Math.abs(l - destL) === 2) {
+        const meio = (l + destL) / 2;
+        if (tabuleiro[meio][c] === 1 && tabuleiro[destL][destC] === 0) {
+            tabuleiro[l][c] = 0;
+            tabuleiro[meio][c] = 0;
+            tabuleiro[destL][destC] = 1;
+            selecionado = null;
+            return true;
+        }
+    }
+    return false;
 }
